57042eea904a95faf11987fc499aa0b34efcbb44
Adrian Oboroc <oboroc@users.noreply.github.com>
2018-08-08 22:54:08 -0400

    Create vs2017 solution

---
+++ src/msxtape.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    [hopefully a] python program for generating wav file form msx cas file
+"""
+
+def msxtape():
+    """ main function """
+    print("msxtape.py")
+
+    for i in range(10):
+        print(i)
+
+    print("lalala")
+
+if __name__ == "__main__":
+    msxtape()
