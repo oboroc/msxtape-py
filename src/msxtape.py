f8a9903ee32801e8db61944fb272be10505c73ff
Adrian Oboroc <oboroc@users.noreply.github.com>
2018-08-15 22:21:41 -0400

    fix missing * in previous commit

--- src/msxtape.py
+++ src/msxtape.py
@@ -3,14 +3,34 @@
     [hopefully a] python program for generating wav file form msx cas file
 """
 
+
 def msxtape():
-    """ main function """
-    print("msxtape.py")
+    """
+    main function - for now just make a 3 second 2.4KHz beep
+    """
 
-    for i in range(10):
-        print(i)
+    import wave, struct, math
 
-    print("lalala")
+#    print(__file__)
+#    print(globals())
+
+    sampleRate = 22050.0    # hertz
+    duration = 3.0          # seconds
+    frequency = 2400.0      # hertz
+
+    wavf = wave.open('file.wav','w')
+    wavf.setnchannels(1)    # mono
+    wavf.setsampwidth(2)    # 16 bit
+    wavf.setframerate(sampleRate)
+
+    for i in range(int(duration * sampleRate)):
+        value = int(32767.0 * math.cos(frequency * math.pi * float(i) / float(sampleRate)))
+        data = struct.pack('<h', value)
+        wavf.writeframes(data)
+
+    wavf.close()
+
+
 
 if __name__ == "__main__":
     msxtape()
