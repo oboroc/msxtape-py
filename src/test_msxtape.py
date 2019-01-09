334221cdf4f51726dced70a75a35546f7ea6e164
Adrian Oboroc <oboroc@users.noreply.github.com>
2019-01-09 17:53:33 -0500

    Update test_msxtape.py

--- src/test_msxtape.py
+++ src/test_msxtape.py
@@ -1,8 +1,17 @@
-import unittest
+import unittest, inspect, msxtape
+
 
 class Test_msxtape(unittest.TestCase):
-    def test_A(self):
+    def test_always_fail(self):
         self.fail("Not implemented")
+
+    def test_8bit_sample_padding(self):
+        f_name = inspect.stack()[0][3] + '.wav'
+        t = msxtape.msxtape(f_name)
+        t.add_tone(1200.0, 1.01)    # 1.01 seconds generates odd number of pcm data bytes
+        l = len(t.pcm_data)
+        del t
+        self.assertEqual(1, 1, 'incorrect default size')
 
 if __name__ == '__main__':
     unittest.main()
